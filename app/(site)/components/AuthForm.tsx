bd47c525624a998bc391f8c0fac637760b550f84
Tharaka Prabhath <tharakaprabhath300@gmail.com>
2024-06-26 00:56:41 +0530

    create signin and login Ui

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -6,6 +6,8 @@ import Input from "@/app/components/inputs/Input";
 import { register } from "module";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle } from "react-icons/bs";
 
 type Varient = 'LOGIN' | "REGISTER"
 const AuthForm = () => {
@@ -31,6 +33,8 @@ const AuthForm = () => {
 
     const onSubmit: SubmitHandler<FieldValues> = (data) => {
         setIsLoading(true);
+
+        
 
         if (variant === 'REGISTER') {
             // axsios register
@@ -60,9 +64,22 @@ const AuthForm = () => {
             <div className="mt-6">
                 <div className="relative ">
                     <div className="absolute inset-0 flex items-center">
-                        <div className="w-full border-t border-gray-300"></div>
+                        <div className="w-full border-t border-gray-300"/>
+                    </div>
+                    <div className="relative flex justify-center text-sm">
+                        <span className="bg-white px-2 text-gray-500">
+                            Or continue with
+                        </span>
                     </div>
                 </div>
+                <div className="mt-6 flex gap-2">
+                    <AuthSocialButton icon={BsGithub} onClick={()=> socialAction('github')}/>
+                    <AuthSocialButton icon={BsGoogle} onClick={()=> socialAction('github')}/>
+                </div>
+            </div>
+            <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+                <div> {variant === 'LOGIN' ? "New to Messenger?":"Alredy have an account?"}</div>
+                <div onClick={togggleVarianet} className="underline cursor-pointer">{variant === 'LOGIN' ? "Create an account":"Login"}</div>
             </div>
         </div>
     </div>);
